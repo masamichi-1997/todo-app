046f97a05cdafabedb5d4981f8afe85d3c0e5a12
masamichi tozawa <fortube7716@gmail.com>
2023-08-27 20:20:09 +0900

    setting head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'TODO APP',
+      htmlAttrs: { lang: 'ja' },
+      meta: [{ name: 'description', content: 'TODO APP' }],
+      link: [{ rel: 'icon', href: 'public/favicon.ico' }],
+    },
+  },
   devtools: { enabled: false },
   modules: ['@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt', ],
   css: ['@/assets/css/global.css'],
